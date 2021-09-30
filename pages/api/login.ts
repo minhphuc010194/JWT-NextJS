6b12d3f4566de15baca8ee7a20bf3880912d5b19
Châu Minh Phúc <58540065+minhphuc010194@users.noreply.github.com>
2021-09-30 22:09:29 +0700

    deploy

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,11 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import jwt from 'jsonwebtoken'
-type Data = {
-  name: string
-}
+
 const KEY = 'djkas90-23jadslsjflasdfljdfklsanmf'
-export default function handler( req: NextApiRequest, res: NextApiResponse<Data>) {
+export default function handler( req: NextApiRequest, res: NextApiResponse) {
  
   if(!req.body){
     res.statusCode = 404
@@ -15,7 +13,7 @@ export default function handler( req: NextApiRequest, res: NextApiResponse<Data>
 
   const { username, password} = req.body
   // res.status(200).json({ name: 'John Doe' })
-  console.log(username, password)
+
   res.json({
     token: jwt.sign({
       username,
